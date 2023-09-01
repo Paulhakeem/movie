381872a715abf6e641f5e0063993a95115bd5103
Paulhakeem <paulnyamawi18@gmail.com>
2023-09-01 23:11:36 +0300

    adding movies

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,8 +1,17 @@
 <script setup>
 import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import api from '../api';
+
+const movie = ref({})
+const route = useRoute()
+
+onBeforeMount(() => {
+    fetch(`https://www.omdbapi.com/?apiKey=${api.apiKey}&i=${route.params.id}&plot=full`)
+})
 </script>
 <template>
     <div>
-   Movie title
+        <h2>Movie title</h2>
     </div>
 </template>
